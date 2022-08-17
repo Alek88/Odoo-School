1522b21d9701bfdf956504433ffab152491b87a2
Aleksander Gurin <gurin.alek@gmail.com>
2022-08-17 08:39:09 +0000

    [IMP] ACCESS: add new access group

--- school_lesson_6_2/models/library_author.py
+++ school_lesson_6_2/models/library_author.py
@@ -1,4 +1,6 @@
+from email.policy import default
 from odoo import fields, models
+from datetime import datetime, timedelta
 
 
 class LibraryAuthor(models.Model):
@@ -8,7 +10,9 @@ class LibraryAuthor(models.Model):
     first_name = fields.Char(required=True)
     last_name = fields.Char(required=True)
     birth_date = fields.Date('Birthday')
-
+    
+    is_write_intern = fields.Boolean(compute='_is_allowed_to_change')
+    
     def name_get(self):
         return [(rec.id, "%s %s" % (
             rec.first_name, rec.last_name)) for rec in self]
@@ -20,3 +24,7 @@ class LibraryAuthor(models.Model):
 
     def _create_by_user(self, vals):
         return self.sudo().create(vals)
+        
+    def _is_allowed_to_change(self):
+        for record in self:
+            record.is_write_intern = record.create_date > datetime.now() - timedelta(days=30)   
